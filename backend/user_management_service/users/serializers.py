5bce82d4dbc204802cdcb96780bb872cb1db4cb1
kasemkl <kmak979@gmail.com>
2024-10-19 13:57:16 +0300

    upload user management service

---
+++ backend/user_management_service/users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'role')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data['role'],
+        )
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'role', 'created_at')
+
