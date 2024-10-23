24d84b9f25999a8e8023423af3a155b63fea7155
unknown <yasserdukmak20@gmail.com>
2024-10-23 16:37:13 +0300

    setup env for Job Post Service

---
+++ backend/Job_Posting_Service/src/test/java/com/example/Job_Posting_Service/JobPostingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Job_Posting_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPostingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
